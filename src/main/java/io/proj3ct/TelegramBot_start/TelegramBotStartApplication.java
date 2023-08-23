27c6a7d2660d9b08235b657769b9f56e2b964b59
test <alexis.cv1824@gmail.com>
2023-08-23 20:34:02 +0300

    Initial commit

---
+++ src/main/java/io/proj3ct/TelegramBot_start/TelegramBotStartApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.TelegramBot_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotStartApplication.class, args);
+	}
+
+}
